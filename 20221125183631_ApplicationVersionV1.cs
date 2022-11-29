bb90ac5ca421376b9dd2e11247716cd46fa49f49
haniel <haniel.arce@gmail.com>
2022-11-29 09:10:18 -0600

    Version 1.0.0.3 commit

---
+++ 20221125183631_ApplicationVersionV1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TRANQ.Server.Infraestructure.Migrations
+{
+    public partial class ApplicationVersionV1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ApplicationPublisehd",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ApplicationUniqueId = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ApplicationName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PublishDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Version = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    IsLast = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ApplicationPublisehd", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ApplicationPublisehd");
+        }
+    }
+}
